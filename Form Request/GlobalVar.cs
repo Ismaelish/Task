313bbe55dc6943265c14d7f277e7b266df5b4944
Ismael Onita <ismaelcorp.pro@gmail.com>
2023-06-12 22:52:24 +0800

    added the orig prod code

---
+++ Form Request/GlobalVar.cs
@@ -0,0 +1,12 @@
+﻿namespace MaximaMachineriesInc
+{
+    public class GlobalVar
+{
+        
+            public static int UserID;
+            public static string UserName;
+            public static string DispName;
+            public static string TblName;
+
+    }
+}
---
+++ Form Request/ViewComponents/CategoryViewComponent.cs
@@ -0,0 +1,25 @@
+﻿using MaximaMachineriesInc;
+using MaximaMachineriesInc.DAL;
+using MaximaMachineriesInc.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SideMenuBar.Models;
+using System.Drawing;
+
+namespace WebApplication1.ViewComponents
+{
+    [ViewComponent(Name = "Category")]
+    public class CategoryViewComponent : ViewComponent
+{
+        private readonly ApplicationDbContext _dbcontext;
+        public CategoryViewComponent (ApplicationDbContext Context)
+        {
+            _dbcontext = Context;
+        }
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var access = _dbcontext.Q_UserMenu.Where(i => i.SystemuserID == GlobalVar.UserID);
+            return View("Index", access.ToList());
+        }
+    }
+}
